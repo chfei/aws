8d8d5229f189d0fad2b27b86a2f2cbbd97a0e4b5
Charles Fei <charles.fei@orionhealth.com>
2017-07-18 18:28:07 +1200

    More test message

---
+++ event-samples/lex16.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "messageVersion": "1.0",
+    "invocationSource": "DialogCodeHook",
+    "userId": "65d2a7d2-5fa9-4b8b-be07-44b32228b2c6:T3SP0BX4Y:U3SPU7URX",
+    "sessionAttributes": {
+        "allAvailableOptions": "[{\"option\":1,\"title\":\"Option 1   CNY2360\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 16:40)  🛬PEK(2017-09-10 19:05) ✈ZH1884 ⏱2H \\n\"}]},{\"option\":2,\"title\":\"Option 2   CNY2580\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 21:15)  🛬PEK(2017-09-10 23:40) ✈MU5186 ⏱2H \\n\"}]},{\"option\":3,\"title\":\"Option 3   CNY2580\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 13:10)  🛬PEK(2017-09-10 15:35) ✈MU564 ⏱2H \\n\"}]},{\"option\":4,\"title\":\"Option 4   CNY2580\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 16:40)  🛬PEK(2017-09-10 19:05) ✈CA1884 ⏱2H \\n\"}]},{\"option\":5,\"title\":\"Option 5   CNY2580\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 09:00)  🛬PEK(2017-09-10 11:25) ✈MU5129 ⏱2H \\n\"}]},{\"option\":6,\"title\":\"Option 6   CNY2580\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 19:10)  🛬PEK(2017-09-10 21:35) ✈CA156 ⏱2H \\n\"}]},{\"option\":7,\"title\":\"Option 7   CNY2580\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 14:55)  🛬PEK(2017-09-10 17:20) ✈MU272 ⏱2H \\n\"}]},{\"option\":8,\"title\":\"Option 8   CNY2840\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 22:10)  🛬PEK(2017-09-11 00:30) ✈HU7610 ⏱2H \\n\"}]},{\"option\":9,\"title\":\"Option 9   CNY3120\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 10:55)  🛬JNG(2017-09-10 12:40) ✈MU9173 ⏱2H \\n\"},{\"value\":\"🛫JNG(2017-09-10 17:15)  🛬PEK(2017-09-10 18:35) ✈MU5703 ⏱1H \\n\"}]},{\"option\":10,\"title\":\"Option 10   CNY4160\",\"fields\":[{\"value\":\"🛫PVG(2017-09-10 06:55)  🛬DLC(2017-09-10 09:00) ✈CZ6529 ⏱2H \\n\"},{\"value\":\"🛫DLC(2017-09-10 10:00)  🛬PEK(2017-09-10 11:30) ✈CZ6129 ⏱2H \\n\"}]}]",
+        "qslots": "{\"ReturnDate\":null,\"Cabin\":\"premium economy\",\"DestinationCity\":\"PEK\",\"DepartureCity\":\"PVG\",\"TripType\":\"one way\",\"Alliance\":\"any\",\"Adults\":\"2\",\"Infants\":\"0\",\"Option\":null,\"Children\":\"0\",\"DepartureDate\":\"2017-09-10\"}",
+        "currentPage": "0"
+    },
+    "bot": {
+        "name": "TravelAgent",
+        "alias": null,
+        "version": "6"
+    },
+    "outputDialogMode": "Text",
+    "currentIntent": {
+        "name": "BookFlight",
+        "slots": {
+            "ReturnDate": null,
+            "Cabin": "premium economy",
+            "DestinationCity": "PEK",
+            "DepartureCity": "PVG",
+            "TripType": "one way",
+            "Alliance": "any",
+            "Adults": "2",
+            "Infants": "0",
+            "Option": "99",
+            "Children": "0",
+            "DepartureDate": "2017-09-10"
+        },
+        "confirmationStatus": "None"
+    },
+    "inputTranscript": "99"
+};
